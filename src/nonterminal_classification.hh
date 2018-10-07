909e24016f045205dda9530a2628fa31d3c3350b
Murto <murto@airmail.cc>
2018-10-07 16:54:03 +0100

    Add nonterminal classification enum class

---
+++ src/nonterminal_classification.hh
@@ -0,0 +1,22 @@
+#pragma once
+
+
+/**
+ * Represents the possible classifications of nonterminals
+**/
+enum class nonterminal_classification
+{
+
+  PROGRAM,
+  COMMAND,
+  LOOP,
+  MOVE_LEFT,
+  MOVE_RIGHT,
+  INCREMENT,
+  DECREMENT,
+  OUTPUT,
+  INPUT,
+  LOOP_BEGIN,
+  LOOP_END
+
+};
