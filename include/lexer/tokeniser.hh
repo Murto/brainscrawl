1e436be3bf91d981c0877732be74643317dbcd1c
Murto <murto@airmail.cc>
2018-10-08 14:13:36 +0100

    fix usage of enum class members and add utility function for creating tokeniser

--- include/lexer/tokeniser.hh
+++ include/lexer/tokeniser.hh
@@ -35,33 +35,40 @@ public:
   /**
    * Returns the next token from the input
   **/
-  token next() const noexcept
+  token next() noexcept
   {
-    using nonterminal_classification;
-
     // Static map of characters to nonterminal classes for tokenisation
     const static std::unordered_map<char, nonterminal_classification> classifications = {
-                                                                                          { '<' , MOVE_LEFT },
-                                                                                          { '>' , MOVE_RIGHT },
-                                                                                          { '+' , INCREMENT },
-                                                                                          { '-' , DECREMENT },
-                                                                                          { '.' , OUTPUT },
-                                                                                          { ',' , INPUT },
-                                                                                          { '[' , LOOP_BEGIN },
-                                                                                          { ']', LOOP_END }
+                                                                                          { '<' , nonterminal_classification::MOVE_LEFT },
+                                                                                          { '>' , nonterminal_classification::MOVE_RIGHT },
+                                                                                          { '+' , nonterminal_classification::INCREMENT },
+                                                                                          { '-' , nonterminal_classification::DECREMENT },
+                                                                                          { '.' , nonterminal_classification::OUTPUT },
+                                                                                          { ',' , nonterminal_classification::INPUT },
+                                                                                          { '[' , nonterminal_classification::LOOP_BEGIN },
+                                                                                          { ']' , nonterminal_classification::LOOP_END }
                                                                                         };
+
+    // Variables for storing the input and location
+    decltype(*_curr) input;
+    decltype(_line) line;
+    decltype(_column) column;
+
     // While the end of input has not been reached
     while (_curr != _end)
     {
 
       // Record current input character and location
-      auto input = *_curr;
-      auto line = _line;
-      auto column = _column;
+      input = *_curr;
+      line = _line;
+      column = _column;
       
       // Increment input iterator
       ++_curr;
 
+      // Newline constant
+      const auto NEWLINE = '\n';
+      
       // Return a token if a valid classification exists
       if (classifications.find(input) != classifications.end())
       {
@@ -84,8 +91,8 @@ public:
 
     }
     
-    // If we have reached end of input then return an EOF token
-    return token(EOF, line, column);
+    // If we have reached end of input then return an EOI token
+    return token(nonterminal_classification::EOI, line, column);
     
   }
   
@@ -99,6 +106,20 @@ private:
   int _column = 1;
 
 };
+
+
+/**
+ * Returns a tokeniser constructed with the given begin and end iterator
+ * Helpful as the user does not have to explicitly define the constructor template arguments
+ *
+ * begin -- The given begin iterator
+ * end   -- The given end iterator
+**/
+template <typename input_it_type>
+tokeniser<input_it_type> make_tokeniser(input_it_type begin, input_it_type end)
+{
+  return tokeniser<input_it_type>(begin, end);
+}
 
 
 }
